735d2881ce3ecf85344c620d98f9b4bd55a1173e
Banu balta <nbanu.balta@gmail.com>
2024-03-24 01:59:12 +0300

    Initial commit

---
+++ src/main/java/com/banubbalta/Kural.java
@@ -0,0 +1,6 @@
+package com.banubbalta;
+
+public interface Kural {
+
+    public boolean saglikDurumu();
+}
---
+++ src/main/java/com/banubbalta/Kus.java
@@ -0,0 +1,13 @@
+package com.banubbalta;
+
+public class Kus extends Hayvan{
+    @Override
+    public void sesVer() {
+
+    }
+
+    @Override
+    public void hareketEt() {
+
+    }
+}
